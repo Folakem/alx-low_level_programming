1fcf3ef24bca0e35c7bb8912661ef3e225c3345f
root <shukuratfolakemi4@gmail.com>
2021-06-28 04:36:08 -0700

    pointer

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,11 +1,12 @@
-/* _putchar function provided by holberton */
-int _putchar(char c);
-
-/* reset to 98 function */
+int _putchar(char);
 void reset_to_98(int *n);
-
-/* swaps integers */
 void swap_int(int *a, int *b);
-
-/* prints the length of a string */
 int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
