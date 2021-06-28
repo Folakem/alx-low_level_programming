68a461988d2f516263ad9e9846d28256362c9d02
root <shukuratfolakemi4@gmail.com>
2021-06-28 08:21:38 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,21 +1,15 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * swap_int - Swap the values of two integers
- *
- * @a: pointer to the first value
- * @b: pointer to the second value
+ * swap_int - swaps the value of two integers
+ * @a: pointer a
+ * @b: pointer b
  */
 void swap_int(int *a, int *b)
 {
-/* make a temporary holding box for variables */
-int c;
+int temp;
 
-/* assign the value of a to c */
-c = *a;
-/* assign the value of b to the address at a */
+temp = *a;
 *a = *b;
-/* assign the c to the address at b */
-*b = c;
+*b = temp;
 }
